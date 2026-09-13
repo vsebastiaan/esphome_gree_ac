a56f830933134242d87d189a7cde0d2fdeb1c64d
vsebastiaan <vsebastiaan@hotmail.com>
2026-09-13 19:00:37 +0200

    feat: add Tosot v5 advanced state model

--- components/tosot_ac/tosot_ac.h
+++ components/tosot_ac/tosot_ac.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 #include <vector>
 
 #include "esphome/components/climate/climate.h"
+#include "esphome/components/select/select.h"
+#include "esphome/components/switch/switch.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/core/component.h"
 #include "esphome/core/gpio.h"
@@ -11,13 +14,36 @@
 namespace esphome {
 namespace tosot_ac {
 
+class TosotACSwitch : public switch_::Switch, public Component {
+ protected:
+  void write_state(bool state) override { this->publish_state(state); }
+};
+
+class TosotACSelect : public select::Select, public Component {
+ protected:
+  void control(const std::string &value) override { this->publish_state(value); }
+};
+
 class TosotAC : public Component, public uart::UARTDevice, public climate::Climate {
  public:
   void setup() override;
   void loop() override;
   void control(const climate::ClimateCall &call) override;
   climate::ClimateTraits traits() override;
+
   void set_kick_pin(InternalGPIOPin *pin) { this->kick_pin_ = pin; }
+
+  void set_horizontal_swing_select(select::Select *value);
+  void set_vertical_swing_select(select::Select *value);
+  void set_display_select(select::Select *value);
+  void set_display_unit_select(select::Select *value);
+
+  void set_turbo_switch(switch_::Switch *value);
+  void set_plasma_switch(switch_::Switch *value);
+  void set_beeper_switch(switch_::Switch *value);
+  void set_sleep_switch(switch_::Switch *value);
+  void set_xfan_switch(switch_::Switch *value);
+  void set_save_switch(switch_::Switch *value);
 
  protected:
   static constexpr uint32_t POLL_INTERVAL_MS = 300;
@@ -34,17 +60,20 @@ class TosotAC : public Component, public uart::UARTDevice, public climate::Clima
   void reset_rx_parser_();
   uint8_t checksum_array_(const uint8_t *data, uint8_t size) const;
   void report_summary_();
+  void log_report_delta_(const std::vector<uint8_t> &frame) const;
 
   void start_kick_(const char *reason);
   void finish_kick_();
   void flush_uart_();
 
+  void queue_control_(const char *reason);
   void send_next_();
   void send_passive_poll_();
   void send_control_(bool af);
   std::vector<uint8_t> build_control_frame_(bool af) const;
   uint8_t checksum_vector_(const std::vector<uint8_t> &frame) const;
   void decode_report_(const std::vector<uint8_t> &frame);
+  void publish_advanced_state_(bool force);
   void log_frame_(const char *prefix, const std::vector<uint8_t> &frame) const;
 
   InternalGPIOPin *kick_pin_{nullptr};
@@ -52,6 +81,18 @@ class TosotAC : public Component, public uart::UARTDevice, public climate::Clima
   uint8_t rx_pos_{0};
   uint8_t rx_expected_{0};
   std::vector<uint8_t> last_report_{};
+
+  select::Select *horizontal_swing_select_{nullptr};
+  select::Select *vertical_swing_select_{nullptr};
+  select::Select *display_select_{nullptr};
+  select::Select *display_unit_select_{nullptr};
+
+  switch_::Switch *turbo_switch_{nullptr};
+  switch_::Switch *plasma_switch_{nullptr};
+  switch_::Switch *beeper_switch_{nullptr};
+  switch_::Switch *sleep_switch_{nullptr};
+  switch_::Switch *xfan_switch_{nullptr};
+  switch_::Switch *save_switch_{nullptr};
 
   uint32_t last_tx_ms_{0};
   uint32_t next_summary_ms_{0};
@@ -63,6 +104,7 @@ class TosotAC : public Component, public uart::UARTDevice, public climate::Clima
   uint32_t rx_byte_count_{0};
   uint32_t rx_frame_count_{0};
   uint32_t rx_report_31_count_{0};
+  uint32_t rx_other_valid_count_{0};
   uint32_t bad_checksum_count_{0};
   uint32_t rx_resync_count_{0};
   uint32_t publish_count_{0};
@@ -70,14 +112,40 @@ class TosotAC : public Component, public uart::UARTDevice, public climate::Clima
 
   bool ready_{false};
   bool state_published_{false};
+  bool advanced_state_initialized_{false};
   bool force_publish_{false};
   bool kick_active_{false};
+
   uint8_t last_mode_code_{1};
   uint8_t last_fan_code_{0};
+
+  uint8_t actual_horizontal_swing_code_{4};
+  uint8_t actual_vertical_swing_code_{9};
+  uint8_t actual_display_mode_{0};
+  bool actual_display_power_{true};
+  bool actual_display_f_{false};
+  bool actual_turbo_{false};
+  bool actual_plasma_{false};
+  bool actual_beeper_{true};
+  bool actual_sleep_{false};
+  bool actual_xfan_{false};
+  bool actual_save_{false};
+
   uint8_t desired_mode_code_{1};
   uint8_t desired_fan_code_{0};
+  uint8_t desired_horizontal_swing_code_{4};
+  uint8_t desired_vertical_swing_code_{9};
+  uint8_t desired_display_mode_{0};
   float desired_target_temperature_{22.0f};
   bool desired_power_{false};
+  bool desired_display_power_{true};
+  bool desired_display_f_{false};
+  bool desired_turbo_{false};
+  bool desired_plasma_{false};
+  bool desired_beeper_{true};
+  bool desired_sleep_{false};
+  bool desired_xfan_{false};
+  bool desired_save_{false};
 
   // 0 = passive polling, 2 = AF control pending, 1 = clear-control pending.
   uint8_t control_stage_{0};
