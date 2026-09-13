266ee0d48cd9531bd5714ec7ab1ef865d3e05e33
vsebastiaan <vsebastiaan@hotmail.com>
2026-09-13 18:20:44 +0200

    feat: configure Tosot RX kick pin

--- components/tosot_ac/climate.py
+++ components/tosot_ac/climate.py
@@ -1,9 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome import pins
 from esphome.components import climate, uart
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ["uart"]
+
+CONF_KICK_PIN = "kick_pin"
 
 tosot_ac_ns = cg.esphome_ns.namespace("tosot_ac")
 TosotAC = tosot_ac_ns.class_("TosotAC", cg.Component, uart.UARTDevice, climate.Climate)
@@ -13,6 +16,7 @@ CONFIG_SCHEMA = cv.All(
     SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(TosotAC),
+            cv.Optional(CONF_KICK_PIN): pins.gpio_output_pin_schema,
         }
     ),
 )
@@ -23,3 +27,7 @@ async def to_code(config):
     await climate.register_climate(var, config)
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
+
+    if CONF_KICK_PIN in config:
+        pin = await cg.gpio_pin_expression(config[CONF_KICK_PIN])
+        cg.add(var.set_kick_pin(pin))
