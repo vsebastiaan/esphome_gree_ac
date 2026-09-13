13d8f9189887629851e8b443302469f66a113e2c
vsebastiaan <vsebastiaan@hotmail.com>
2026-09-13 16:29:25 +0200

    fix: declare Tosot climate id correctly

--- components/tosot_ac/climate.py
+++ components/tosot_ac/climate.py
@@ -1,17 +1,25 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate, uart
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ["uart"]
 
 tosot_ac_ns = cg.esphome_ns.namespace("tosot_ac")
 TosotAC = tosot_ac_ns.class_("TosotAC", cg.Component, uart.UARTDevice, climate.Climate)
 
-CONFIG_SCHEMA = climate.climate_schema(TosotAC).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
+SCHEMA = climate.climate_schema(climate.Climate).extend(uart.UART_DEVICE_SCHEMA)
+CONFIG_SCHEMA = cv.All(
+    SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(TosotAC),
+        }
+    ),
+)
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[cv.GenerateID().schema])
+    var = cg.new_Pvariable(config[CONF_ID])
     await climate.register_climate(var, config)
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
