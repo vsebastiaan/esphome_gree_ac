eecc5e04e699e449d5676b0f9013679aa6cc72d7
vsebastiaan <vsebastiaan@hotmail.com>
2026-09-13 17:02:23 +0200

    fix: use proven replay RX parser in Tosot climate driver

--- components/tosot_ac/tosot_ac.h
+++ components/tosot_ac/tosot_ac.h
@@ -19,29 +19,37 @@ class TosotAC : public Component, public uart::UARTDevice, public climate::Clima
 
  protected:
   static constexpr uint32_t POLL_INTERVAL_MS = 300;
-  static constexpr size_t FRAME_MAX = 80;
+  static constexpr uint32_t SUMMARY_INTERVAL_MS = 5000;
+  static constexpr uint8_t RX_BUFFER_SIZE = 64;
 
-  void read_uart_();
-  void consume_byte_(uint8_t value);
-  void finish_frame_();
-  bool checksum_ok_(const std::vector<uint8_t> &frame) const;
-  uint8_t checksum_(const std::vector<uint8_t> &frame) const;
+  // RX path intentionally mirrors the known-good gree_replay implementation.
+  void consume_rx_byte_(uint8_t value);
+  void finish_rx_frame_();
+  void reset_rx_parser_();
+  uint8_t checksum_array_(const uint8_t *data, uint8_t size) const;
+  void report_summary_();
 
   void send_next_();
   void send_passive_poll_();
   void send_control_(bool af);
   std::vector<uint8_t> build_control_frame_(bool af) const;
+  uint8_t checksum_vector_(const std::vector<uint8_t> &frame) const;
   void decode_report_(const std::vector<uint8_t> &frame);
   void log_frame_(const char *prefix, const std::vector<uint8_t> &frame) const;
 
-  std::vector<uint8_t> rx_frame_{};
-  size_t rx_expected_{0};
+  uint8_t rx_buffer_[RX_BUFFER_SIZE]{};
+  uint8_t rx_pos_{0};
+  uint8_t rx_expected_{0};
   std::vector<uint8_t> last_report_{};
 
   uint32_t last_tx_ms_{0};
+  uint32_t next_summary_ms_{0};
   uint32_t tx_count_{0};
-  uint32_t rx_count_{0};
+  uint32_t rx_byte_count_{0};
+  uint32_t rx_frame_count_{0};
+  uint32_t rx_report_31_count_{0};
   uint32_t bad_checksum_count_{0};
+  uint32_t rx_resync_count_{0};
 
   bool ready_{false};
   uint8_t last_mode_code_{1};
